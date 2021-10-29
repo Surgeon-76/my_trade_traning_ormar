d4163d520d6cf4ba0182f939914263093ebfbd6d
Surgeon76 <Surgeon76@yandex.ru>
2021-10-29 23:48:32 +0300

    :-)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
 
-app = FastAPI()
-
+app = FastAPI(
+    title="Супер-пупер МеГаШоП v 2.2E+5",
+    description="Второй блин не лучше первого...- комкуется не хуже)))"
+)
