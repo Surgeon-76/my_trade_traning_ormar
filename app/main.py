b5daa0befe103c0a925ad6a448707d45097ec6c6
Surgeon76 <Surgeon76@yandex.ru>
2021-10-30 00:01:50 +0300

    1 day

--- app/main.py
+++ app/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
 
+from app.routers import (
+    customers,
+    items,
+    orders,
+    orderitems
+)
+from app.settings.db import (
+    database,
+    metadata,
+    engine
+)
 
 app = FastAPI(
     title="Супер-пупер МеГаШоП v 2.2E+5",
     description="Второй блин не лучше первого...- комкуется не хуже)))"
 )
+
+metadata.create_all(engine)
+
+app.state.database = database
+
+# app.include_router(customers.customers_route)
+# app.include_router(orders.order_route)
+# app.include_router(items.items_route)
+# app.include_router(orderitems.links_route)
