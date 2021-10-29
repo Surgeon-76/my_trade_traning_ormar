93d9a8f30adad8db4ce9d0eaca0ed99561ffc6fe
Surgeon76 <Surgeon76@yandex.ru>
2021-10-29 23:37:35 +0300

    Создана схема сервиса

---
+++ app/settings/db.py
@@ -0,0 +1,14 @@
+from databases import Database
+from sqlalchemy import create_engine
+from sqlalchemy.sql.schema import MetaData
+
+user_name_db = 'surglin'
+password_db = 'Nusha230399'
+db_name = 'my_shop_db'
+
+URL_DATA_BASE = (
+    f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}"
+)
+database = Database(URL_DATA_BASE)
+engine = create_engine(URL_DATA_BASE)
+metadata = MetaData()
