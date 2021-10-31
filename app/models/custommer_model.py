a25f5ba7c00d692f7d65c8286da28e593239f638
Surgeon76 <Surgeon76@yandex.ru>
2021-10-31 18:56:51 +0300

    Model Customer

---
+++ app/models/custommer_model.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import databases
+import sqlalchemy
+
+import ormar
+
+from app.settings.db import (
+    database, 
+    metadata
+)
+
+
+class Cusomer(ormar.Model):
+    class Meta:
+        database = database
+        metadata = metadata
+
+    id = ormar.Integer(primary_key=True, index=True)
+    first_name = ormar.String(max_length=100, index=True)
+    last_name = ormar.String(max_length=100, index=True)
+    username = ormar.String(max_length=50,index=True)
+    email = ormar.String(max_length=200, index=True)
+    hasheed_password = ormar.String(max_length=50, index=True)
+    created_on = ormar.DateTime(default=datetime.now, index=True)
+    updated_on = ormar.DateTime(default=datetime.now, onupdate=datetime.now)
