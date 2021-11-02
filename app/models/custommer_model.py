7ca7b93fa683910772eae5c0be8194da3760b198
Surgeon76 <Surgeon76@yandex.ru>
2021-11-02 23:19:02 +0300

    Item Order

--- app/models/custommer_model.py
+++ app/models/custommer_model.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-import ormar
-from sqlalchemy.sql.schema import Index
+from typing import Optional
 
+import ormar
+
+from app.models.order_model import Order
 from app.settings.db import (
     database, 
     metadata
@@ -18,7 +20,7 @@ class Customer(ormar.Model):
     last_name: str = ormar.String(max_length=100, index=True)
     username: str = ormar.String(max_length=50, Index=True)
     email: str = ormar.String(max_length=200, index=True)
-    hasheed_password: str = ormar.String(max_length=50)
+    hashed_password: str = ormar.String(max_length=50)
     created_on: datetime = ormar.DateTime(default=datetime.now(), index=True)
     updated_on: datetime = ormar.DateTime(default=datetime.now())
-    
+    orders: Optional[Order] = ormar.ForeignKey(Order)
---
+++ app/models/item_model.py
@@ -0,0 +1,19 @@
+import decimal
+import ormar
+from sqlalchemy.sql.schema import Index
+
+from app.settings.db import (
+    database,
+    metadata
+)
+class Item(ormar.Model):
+    class Meta:
+        database = database
+        metadata = metadata
+
+    id: int = ormar.Integer(primary_key=True, Index=True)
+    name: str = ormar.String(max_length=200, Index=True)
+    cost_price: float = ormar.Decimal(scale=10, precision=2)
+    selling_price: decimal = ormar.Decimal(scale=10, precision=2)
+    quantity: int = ormar.Integer()
+    
