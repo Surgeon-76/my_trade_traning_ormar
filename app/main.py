33daddbac7c30209ac8a5700efea8a611d84db32
Surgeon76 <Surgeon76@yandex.ru>
2021-11-14 00:38:29 +0300

    format

--- app/main.py
+++ app/main.py
@@ -23,12 +23,12 @@ metadata.create_all(engine)
 
 app.state.database = database
 
+
 @app.on_event("startup")
 async def startup() -> None:
     database_ = app.state.database
     if not database_.is_connected:
         await database_.connect()
-
 
 @app.on_event("shutdown")
 async def shutdown() -> None:
